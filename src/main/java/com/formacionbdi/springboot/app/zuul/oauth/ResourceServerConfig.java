8d637d84eada5787c1b821f64651946a08b1f5fe
luis <l.miguel0529@hotmaail.com>
2020-09-18 15:13:01 -0500

    Add docker

--- src/main/java/com/formacionbdi/springboot/app/zuul/oauth/ResourceServerConfig.java
+++ src/main/java/com/formacionbdi/springboot/app/zuul/oauth/ResourceServerConfig.java
@@ -34,9 +34,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/api/security/oauth/**").permitAll()
-		.antMatchers(HttpMethod.GET, "/api/productos/listar", "/api/items/listar", "/api/usuarios/usuarios").permitAll()
-		.antMatchers(HttpMethod.GET, "/api/productos/ver/{id}", 
-				"/api/items/ver/{id}/cantidad/{cantidad}", 
+		.antMatchers(HttpMethod.GET, "/api/productos/list", "/api/items/list", "/api/usuarios/usuarios").permitAll()
+		.antMatchers(HttpMethod.GET, "/api/productos/list/{id}",
+				"/api/items/list/{id}/cantidad/{cantidad}",
 				"/api/usuarios/usuarios/{id}").hasAnyRole("ADMIN", "USER")
 		.antMatchers("/api/productos/**", "/api/items/**", "/api/usuarios/**").hasRole("ADMIN")
 		.anyRequest().authenticated();
